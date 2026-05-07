5077155bc3509f23b31a0e021234340325695031
harishbalan2101 <hb3398@srmist.edu.in>
2026-05-07 15:04:03 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,21 +1,17 @@
-
 public class TicTacToe {
 
     public static void main(String[] args) {
+        int slot = 7;
 
-        char[][] board = new char[3][3];
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
