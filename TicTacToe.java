ed21371a759a808617efc3ebad3d7187b373b4ca
harishbalan2101 <hb3398@srmist.edu.in>
2026-05-07 15:00:41 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,21 +1,32 @@
 
-public class TicTacToe {
+import java.util.*;
+
+public class TicTacToe{
 
     public static void main(String[] args) {
 
-        char[][] board = new char[3][3];
+        Random rand = new Random();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        int toss = rand.nextInt(2); // 0 or 1
+
+        char player1Symbol, player2Symbol;
+        String currentPlayer;
+
+        if (toss == 0) {
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayer = "Player 1";
+        } else {
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+            currentPlayer = "Player 2";
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Player 1 Symbol: " + player1Symbol);
+        System.out.println("Player 2 Symbol: " + player2Symbol);
+        System.out.println("First Turn: " + currentPlayer);
     }
 }
+
+
+    
